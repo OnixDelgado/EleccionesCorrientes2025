7438d5ce6b6fd6472d03bdf96aafc5e5885ffc89
Onix Delgado <onixdelgado95@gmail.com>
2025-03-03 20:42:18 -0300

    Prueba 5- App instalable

--- script.js
+++ script.js
@@ -150,8 +150,8 @@ document.addEventListener('fullscreenchange', updateFullscreenButton);
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
       navigator.serviceWorker.register('/sw.js')
-        .then(reg => console.log('Service Worker registrado', reg))
-        .catch(err => console.log('Error al registrar el Service Worker', err));
+        .then(reg => console.log('Service Worker registrado con éxito:', reg))
+        .catch(err => console.log('Error al registrar el Service Worker:', err));
     });
   }
   
