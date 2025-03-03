fff6a8800cf59dad12ec2f020333f29577971d0e
Onix Delgado <onixdelgado95@gmail.com>
2025-03-03 20:52:27 -0300

    Volver a la prueba 3

--- script.js
+++ script.js
@@ -148,10 +148,9 @@ if (fullscreenButton) {
 document.addEventListener('fullscreenchange', updateFullscreenButton);
 
 if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/sw.js')
-        .then(reg => console.log('Service Worker registrado con éxito:', reg))
-        .catch(err => console.log('Error al registrar el Service Worker:', err));
-    });
-  }
-  
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(reg => console.log('Service Worker registrado', reg))
+      .catch(err => console.log('Error al registrar el Service Worker', err));
+  });
+}
--- sw.js
+++ sw.js
@@ -7,36 +7,31 @@ const urlsToCache = [
   '/img/candi-1.png',
   '/img/candi-2.png',
   '/img/candi-3.png',
-  '/img/candi-4.png',
-  '/manifest.json'
+  '/img/candi-4.png'
 ];
 
-// Instalación del Service Worker
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Activación y eliminación de cachés antiguas
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.filter(cache => cache !== CACHE_NAME)
-          .map(cache => caches.delete(cache))
-      );
-    })
-  );
-});
-
-// Intercepción de peticiones
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
       return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(cacheName => cacheName !== CACHE_NAME)
+          .map(cacheName => caches.delete(cacheName))
+      );
     })
   );
 });
